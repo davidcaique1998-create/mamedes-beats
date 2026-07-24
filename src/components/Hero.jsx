fb5284bfd2d11b8e42aa5a4f99a7ec3943850285
Gabriel <davidcaique1998@gmail.com>
2026-07-24 20:48:46 -0300

    atualizar textos do hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,13 @@ export default function Hero() {
         <p className="subtitle">PRODUÇÃO MUSICAL</p>
 
         <h1>
-          BEATS QUE <span>VENDEM</span>
+          BEATS QUE <span>ENTRAM NA MENTE</span>
         </h1>
 
         <p>
           Encontre beats exclusivos e licenciados para elevar o nível das suas
-          músicas. Qualidade profissional, entrega instantânea e licenças
-          seguras.
+          músicas. Sons diferenciados, personalização e licenças
+  seguras.
         </p>
 
         <div className="buttons">
